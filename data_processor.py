d3545ec3dede777b1691ae07061905ef620c6f5c
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-09-04 18:01:09 +0300

    fixed column names, cleaned nan values

--- data_processor.py
+++ data_processor.py
@@ -11,6 +11,7 @@ load_dotenv()
 list_of_countries = constants.COUNTRIES_LIST
 final_df = pd.DataFrame()
 files = [os.getenv('SURVEY_2022'), os.getenv('SURVEY_2023')]
+column_name_mappings = {"LanguageHaveWorkedWith": "Language"}
 
 for file in files:
     df = pd.read_csv(file)
@@ -23,14 +24,22 @@ for file in files:
     df = df['LanguageHaveWorkedWith'].str.split(';').explode()
     df = df.value_counts().reset_index()
 
+    filename = f"Survey_{file[-8:-4]}"
+
     if final_df.empty:
         final_df = df
+        name_of_last_column = final_df.columns[-1]
+        final_df.rename(columns={name_of_last_column: filename}, inplace=True)
     else:
         final_df = final_df.merge(df, on='LanguageHaveWorkedWith', how='outer')
+        name_of_last_column = final_df.columns[-1]
+        final_df.rename(columns={name_of_last_column: filename}, inplace=True)
 
 
-final_df['count_x'].fillna(0, inplace=True)
-final_df['count_y'].fillna(0, inplace=True)
+final_df.rename(columns=column_name_mappings, inplace=True)
+final_df.fillna(0, inplace=True)
 print(final_df.head(10))
 print('----------------------')
 print(final_df.tail(10))
+print('----------------------')
+
