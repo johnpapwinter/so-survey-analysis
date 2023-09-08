05ec434378a3a78d59e1dabe36f1ed19e756ffbd
johnpapwinter <johnpapahimonas@yahoo.com>
2023-09-08 15:04:21 +0300

    removed unneeded constants

--- constants.py
+++ constants.py
@@ -5,56 +5,5 @@ COUNTRIES_LIST = ['France', 'Ireland', 'Germany', 'Netherlands', 'Canada', 'Gree
                   'Portugal', 'Turkey', 'Croatia', 'Poland', 'Czech Republic', 'Italy', 'Norway', 'Sweden',
                   'Denmark', 'Israel']
 
-COLUMNS_SURVEY_21_22_23_LIST = ['Employment',
-                                'Country',
-                                'LanguageHaveWorkedWith',
-                                'DatabaseHaveWorkedWith',
-                                'WebframeHaveWorkedWith']
-
-COLUMNS_SURVEY_19_20_LIST = ['Employment',
-                             'Country',
-                             'LanguageWorkedWith',
-                             'DatabaseWorkedWith',
-                             'WebframeWorkedWith']
-
-COLUMNS_SURVEY_18_LIST = ['Employment',
-                          'Country',
-                          'LanguageWorkedWith',
-                          'DatabaseWorkedWith',
-                          'FrameworkWorkedWith']
-
-COLUMNS_SURVEY_17_LIST = ['EmploymentStatus',
-                          'Country',
-                          'HaveWorkedLanguage',
-                          'HaveWorkedFramework',
-                          'HaveWorkedDatabase']
-
 COUNTRY = 'Country'
 
-EMPLOYMENT_18_19_20_21_22_23 = 'Employment'
-
-EMPLOYMENT_17 = 'EmploymentStatus'
-
-EMPLOYMENT_SURVEY_17_18_19_20_21 = 'Employed full-time'
-
-EMPLOYMENT_SURVEY_22_23 = 'Employed, full-time'
-
-PROGRAMMING_LANGUAGES_17 = 'HaveWorkedLanguage'
-
-PROGRAMMING_LANGUAGES_18_19_20 = 'LanguageWorkedWith'
-
-PROGRAMMING_LANGUAGES_21_22_23 = 'LanguageHaveWorkedWith'
-
-WEB_FRAMEWORKS_17 = 'HaveWorkedFramework'
-
-WEB_FRAMEWORKS_18 = 'FrameworkWorkedWith'
-
-WEB_FRAMEWORKS_19_20 = 'WebframeWorkedWith'
-
-WEB_FRAMEWORKS_21_22_23 = 'WebframeHaveWorkedWith'
-
-DATABASES_17 = 'HaveWorkedDatabase'
-
-DATABASES_18_19_20 = 'DatabaseWorkedWith'
-
-DATABASES_21_22_23 = 'DatabaseHaveWorkedWith'
