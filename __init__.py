b8f2d0bb29f1f42e7176b12f98a4e36f2058fdc0
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-09-12 00:19:23 +0300

    code cleaning

--- __init__.py
+++ __init__.py
@@ -2,35 +2,25 @@ import os
 import pandas as pd
 import constants
 from dotenv import load_dotenv
-from languages_cleaner import generate_answer_counter
+from data_processor import analyze_survey_language
 from excel_service import export_to_excel
 
 load_dotenv()
 
-# GET SURVEY FILENAME AND CREATE DATAFRAME
-csv_filename = os.getenv('SURVEY_2023')
-df = pd.read_csv(csv_filename)
-
-
-# FILTER THE DATAFRAME AND KEEP ONLY THE DESIRED COLUMNS
-columns_to_keep = ['Employment', 'Country', 'LanguageHaveWorkedWith',
-                   'DatabaseHaveWorkedWith', 'WebframeHaveWorkedWith']
-filtered_df = df[columns_to_keep]
-
-# FILTER THE DATAFRAME ONLY FOR THE COUNTRY NEEDED AND FULL-TIME EMPLOYMENT
 list_of_countries = constants.COUNTRIES_LIST
-target_employment = 'Employed, full-time'
-
+files = [os.getenv('SURVEY_2023'),
+         os.getenv('SURVEY_2022'),
+         os.getenv('SURVEY_2021'),
+         os.getenv('SURVEY_2020'),
+         os.getenv('SURVEY_2019'),
+         os.getenv('SURVEY_2018')]
 
 for country in list_of_countries:
-    temp_df = filtered_df[
-        (filtered_df['Country'] == country) & (filtered_df['Employment'] == target_employment)]
+    print(f"RESULTS FOR {country.upper()}")
+    analyze_survey_language(country, files)
+    print('----------------------')
+    print('----------------------')
+    print('----------------------')
 
-    total_people = temp_df.shape[0]
-    print(f"--------FOR {country.upper()}---------")
-    print(generate_answer_counter(temp_df, 'LanguageHaveWorkedWith', total_people))
-    print(generate_answer_counter(temp_df, 'WebframeHaveWorkedWith', total_people))
-    print(generate_answer_counter(temp_df, 'DatabaseHaveWorkedWith', total_people))
-    print(f"-----WITH A NUMBER OF RESPONDENTS: {total_people}----")
-    print("====================================")
+
 
--- data_processor.py
+++ data_processor.py
@@ -1,51 +1,40 @@
-import os
 import pandas as pd
-import constants
 from dotenv import load_dotenv
 from languages_cleaner import clean_languages_dataframe
 
 load_dotenv()
 
 
-list_of_countries = constants.COUNTRIES_LIST
-final_df = pd.DataFrame()
-files = [os.getenv('SURVEY_2023'), os.getenv('SURVEY_2022'), os.getenv('SURVEY_2021'), os.getenv('SURVEY_2020'),
-         os.getenv('SURVEY_2019'), os.getenv('SURVEY_2018')]
-column_name_mappings = {"LanguageHaveWorkedWith": "Language"}
+def analyze_survey_language(country: str, files):
+    final_df = pd.DataFrame()
+    column_name_mappings = {"LanguageHaveWorkedWith": "Language"}
 
-for file in files:
-    df = pd.read_csv(file)
+    for file in files:
+        df = pd.read_csv(file)
 
-    target_employment = 'Employed, full-time'
-    df = df[(df['Country'] == 'Greece') & (df['Employment'] == target_employment)]
+        target_employment = 'Employed, full-time'
+        df = df[(df['Country'] == country) & (df['Employment'] == target_employment)]
 
-    total_respondents = df.shape[0]
-    df = df['LanguageHaveWorkedWith'].str.split(';').explode()
-    df = df.value_counts().reset_index()
+        total_respondents = df.shape[0]
+        df = df['LanguageHaveWorkedWith'].str.split(';').explode()
+        df = df.value_counts().reset_index()
 
-    df[df.columns[-1]] = (df[df.columns[-1]] / total_respondents) * 100
-    df = clean_languages_dataframe(df)
-    df['LanguageHaveWorkedWith'] = df['LanguageHaveWorkedWith'].str.lower()
+        df[df.columns[-1]] = (df[df.columns[-1]] / total_respondents) * 100
+        df = clean_languages_dataframe(df)
+        df['LanguageHaveWorkedWith'] = df['LanguageHaveWorkedWith'].str.lower()
 
-    filename = f"Survey_{file[-8:-4]}"
+        filename = f"Survey_{file[-8:-4]}"
 
-    if final_df.empty:
-        final_df = df
-        name_of_last_column = final_df.columns[-1]
-        final_df.rename(columns={name_of_last_column: filename}, inplace=True)
-    else:
-        final_df = final_df.merge(df, on='LanguageHaveWorkedWith', how='outer')
+        if final_df.empty:
+            final_df = df
+            name_of_last_column = final_df.columns[-1]
+            final_df.rename(columns={name_of_last_column: filename}, inplace=True)
+        else:
+            final_df = final_df.merge(df, on='LanguageHaveWorkedWith', how='outer')
 
-        name_of_last_column = final_df.columns[-1]
-        final_df.rename(columns={name_of_last_column: filename}, inplace=True)
+            name_of_last_column = final_df.columns[-1]
+            final_df.rename(columns={name_of_last_column: filename}, inplace=True)
 
-
-final_df.rename(columns=column_name_mappings, inplace=True)
-final_df.fillna(0, inplace=True)
-print('----------------------')
-# print(final_df.head(10))
-print('----------------------')
-# print(final_df.tail(10))
-print('----------------------')
-print(final_df.to_string(index=True))
-print('----------------------')
+    final_df.rename(columns=column_name_mappings, inplace=True)
+    final_df.fillna(0, inplace=True)
+    print(final_df.to_string(index=True))
